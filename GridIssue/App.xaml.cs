856c736d86a02c43b3bb95cbb88145a076db8269
Satoshi Nakamura <kamuxxx@hotmail.com>
2023-04-27 12:20:52 +0900

    First commit

---
+++ GridIssue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GridIssue;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
+
---
+++ GridIssue/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿namespace GridIssue;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        mainLabel.Text = @"2Columns / RowHeight Default BR
+TextTextTextTextTextTextTextTextTextTextTextTextTextTextBR
+TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextBR
+TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextBR
+TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextEnd";
+
+        mainLabel2.Text = @"2Columns / RowHeight Auto BR
+TextTextTextTextTextTextTextTextTextTextTextTextTextTextBR
+TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextBR
+TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextBR
+TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextEnd";
+
+        mainLabel3.Text = @"1Column BR
+TextTextTextTextTextTextTextTextTextTextTextTextTextTextBR
+TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextBR
+TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextBR
+TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextEnd";
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        
+
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
+
+
